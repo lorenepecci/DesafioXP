fbf1ec642b0ebc7e8c225a221b7f4801e2862a2e
Lorene Pecci <lorenepecci@gmail.com>
2022-07-15 13:43:31 -0300

    schema prisma done

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import express from 'express';
 import "dotenv/config";
 
 const app = express();
-const { PORT } = process.env;
-app.listen(PORT, ()=> console.log(`Escutando na porta ${PORT}`))
+const { PORT_SERVER } = process.env;
+app.listen(PORT_SERVER, ()=> console.log(`Escutando na porta ${PORT_SERVER}`))
