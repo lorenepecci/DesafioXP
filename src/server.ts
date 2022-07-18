0e30d9740d2bed8549df779c3ae3ed9073aacca4
Lorene Pecci <lorenepecci@gmail.com>
2022-07-17 21:30:59 -0300

    swagger initiated

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
+import 'dotenv/config';
+import 'fs';
+import swaggerUi from 'swagger-ui-express';
 import { app } from './app';
-import "dotenv/config";
+
+const swaggerDocument = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const { PORT_SERVER } = process.env;
-app.listen(PORT_SERVER, ()=> console.log(`Escutando na porta ${PORT_SERVER}`))
+app.listen(PORT_SERVER, () => console.log(`Escutando na porta ${PORT_SERVER}`));
