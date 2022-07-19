f68aaf892316e81b210e20753a65432a40fcc6aa
Lorene Pecci <lorenepecci@gmail.com>
2022-07-19 09:54:08 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
+import cors from 'cors';
 import express from 'express';
 import 'express-async-errors';
 import erroHttp from './middlewares/error';
 import { router } from './routes';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(router);
 app.use(erroHttp);
