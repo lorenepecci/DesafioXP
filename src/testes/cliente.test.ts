4491ac4c8def0221f13da643573741010f42f63c
Lorene Pecci <lorenepecci@gmail.com>
2022-07-20 22:07:05 -0300

    test at route clientes done

--- src/testes/cliente.test.ts
+++ src/testes/cliente.test.ts
@@ -8,15 +8,15 @@ describe('--- Testes na rota /clientes ---', () => {
   describe('--- Método POST na rota /clientes ---', () => {
     const postClienteSucesso: ICliente = {
       nome: 'fulano4',
-      senha: 'fu123456',
+      senha: '123456',
       email: 'fulano4@gmail.com',
     };
 
     it('Deve ser possivel cadastrar um cliente', async () => {
       const response = await request(app)
         .post('/clientes')
-        .send( postClienteSucesso );
-      
+        .send(postClienteSucesso);
+
       expect(response.status).to.be.equal(200);
       expect(response.body).to.have.all.keys([
         'codCliente',
@@ -25,11 +25,62 @@ describe('--- Testes na rota /clientes ---', () => {
         'email',
         'saldo',
       ]);
-      expect(response.body.nome).to.be.an('string');
-      expect(response.body.senha).to.be.an('string');
-      expect(response.body.email).to.be.an('string');
-      expect(response.body.codCliente).to.be.an('number');
-      expect(response.body.saldo).to.be.an('string');
+    });
+    it('Erro se o cliente já foi cadastrado ', async () => {
+      const response = await request(app)
+        .post('/clientes')
+        .send(postClienteSucesso);
+
+      expect(response.status).to.be.equal(400);
+      expect(response.body).to.have.key('message');
+      expect(response.body.message).to.be.equal(
+        'Este usuário já foi cadastrado.'
+      );
+    });
+
+    it('Erro se não há o campo nome ', async () => {
+      const response = await request(app)
+        .post('/clientes')
+        .send({ senha: 'fu123456', email: 'fulano4@gmail.com' });
+
+      expect(response.status).to.be.equal(400);
+      expect(response.body).to.have.key('message');
+      expect(response.body.message).to.be.equal('"nome" is required');
+    });
+
+    it('Erro se não há o campo senha ', async () => {
+      const response = await request(app).post('/clientes').send({
+        nome: 'fulano4',
+        email: 'fulano4@gmail.com',
+      });
+
+      expect(response.status).to.be.equal(400);
+      expect(response.body).to.have.key('message');
+      expect(response.body.message).to.be.equal('"senha" is required');
+    });
+    it('Erro se não há o campo email ', async () => {
+      const response = await request(app).post('/clientes').send({
+        nome: 'fulano4',
+        senha: 'fu123456',
+      });
+
+      expect(response.status).to.be.equal(400);
+      expect(response.body).to.have.key('message');
+      expect(response.body.message).to.be.equal('"email" is required');
+    });
+
+    it('Erro se o email nao tem formato valido', async () => {
+      const response = await request(app).post('/clientes').send({
+        email: 'fulano',
+        nome: 'fulano4',
+        senha: 'fu123456',
+      });
+
+      expect(response.status).to.be.equal(422);
+      expect(response.body).to.have.key('message');
+      expect(response.body.message).to.be.equal(
+        '"email" must be a valid email'
+      );
     });
   });
 });
